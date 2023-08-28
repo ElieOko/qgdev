ffb38bbd2b03dfab9c9484416bedb0680047675d
Elie Oko <elieoko100@gmail.com>
2023-08-28 10:52:42 +0100

    build:project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
 import '@/styles/globals.css'
+import ProgressBar from '@badrap/bar-of-progress'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { Router } from 'next/router'
+import { ComponentType, ReactElement, ReactNode } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const progress = new ProgressBar({
+  size: 2,
+  color: '#38bdf8',
+  className: 'bar-of-progress',
+  delay: 100,
+})
+//checking windows
+if (typeof window !== 'undefined') {
+  progress.start()
+  progress.finish()
 }
+//event router
+Router.events.on('routeChangeStart', () => progress.start())
+Router.events.on('routeChangeComplete', () => progress.finish())
+Router.events.on('routeChangeError', () => progress.finish())
+export type NextPageWithLayout<P = { props?: any }> = NextPage<P> & {
+  getLayout?: (_page: ReactElement) => ReactNode;
+  layout?: ComponentType;
+};
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+export default function App({ Component, pageProps,router }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return(
+      <>
+        {getLayout(<Component {...pageProps} />)}
+      </>
+  )
+}
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
